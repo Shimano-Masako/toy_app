032956e433ec5d84c2c08f56d28b8b30bb0b78d4
島野雅子 <b25119ms@aiit.ac.jp>
2025-04-13 06:32:53 +0000

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :microposts
   validates :content, presence: true # （コードを書き込む）の中身を書き換えてください
-  validates length: { maximum: 140 }, presence: true # （コードを書き込む）の中身を書き換えてください
 end
